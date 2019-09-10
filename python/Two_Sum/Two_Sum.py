e78bea65efaa50c7d3231bee649a737751810240
Bryce <33057792+Everglowww@users.noreply.github.com>
2019-09-10 10:16:24 +0800

    Update Two_Sum.py

--- python/Two_Sum/Two_Sum.py
+++ python/Two_Sum/Two_Sum.py
@@ -4,13 +4,13 @@
 # Author: Bryce
 
 class Solution:
-    def twoSum ( self , nums, target ):
+    def twoSum (self ,nums,target ):
     dic={ };
-    for index, num in enumerate ( nums ):
+    for index, num in enumerate (nums):
         if num in dic :
-            return  [ dic [ num] ], index ]
-        dic [ target - num ]= index
+            return  [dic[num]], index]
+        dic [target - num]= index
 if __name__ == " __main__ ":
     nums = [ 2 , 7 , 11 , 15 ]
     target = 9
-    print ( Solution( ).twoSum ( nums, target ) )
+    print ( Solution().twoSum (nums, target) )
