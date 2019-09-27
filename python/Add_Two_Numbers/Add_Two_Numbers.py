3834877e686a1ff6d783ff797c69de2b160f684e
Bryce <33057792+Everglowww@users.noreply.github.com>
2019-09-27 16:59:16 +0800

    Update Add_Two_Numbers.py

--- python/Add_Two_Numbers/Add_Two_Numbers.py
+++ python/Add_Two_Numbers/Add_Two_Numbers.py
@@ -6,37 +6,6 @@ class ListNode(object):
 
 
 class Solution(object):
-    # def addTwoNumbers(self, l1, l2):
-    #     """
-    #     :type l1: ListNode
-    #     :type l2: ListNode
-    #     :rtype: ListNode
-    #     """
-    #     last = 0
-    #     head = prev = None
-    #     while True:
-    #         if l2 is None and l1 is None and last == 0:
-    #             break
-    #         val = last
-    #         if l2 is not None:
-    #             val += l2.val
-    #             l2 = l2.next
-    #         if l1 is not None:
-    #             val += l1.val
-    #             l1 = l1.next
-    #         if val >= 10:
-    #             val = val % 10
-    #             last = 1
-    #         else:
-    #             last = 0
-    #         current = ListNode(val)
-    #         if prev is None:
-    #             head = current
-    #         else:
-    #             prev.next = current
-    #         prev = current
-    #     return head
-
     def addTwoNumbers(self, l1, l2):
         carry = 0
         # dummy head
