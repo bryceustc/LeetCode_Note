f2d0bddd874209cc671b41ab9d42efcb988f67ab
Bryce <33057792+Everglowww@users.noreply.github.com>
2019-10-10 21:57:42 +0800

    Update Longest_Substring_Without_Repeating_Characters.cpp

--- cpp/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.cpp
+++ cpp/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.cpp
@@ -1,1 +1,53 @@
+/// Source : https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+/// Author : bryce
+/// Time   : 2019-10-10
+
 #include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+// Sliding Window
+// Time Complexity: O(len(s))
+// Space Complexity: O(len(charset))
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int freq[256] = {0};
+
+        int l = 0, r = -1; // sliding window: s[l...r]
+        int res = 0;
+
+        while(r + 1 < s.size()){
+            //cout << s[r+1] << endl;
+            //cout << freq[s[r+1]] << endl;
+            if( freq[s[r + 1]] == 0 )
+            {
+               r++;
+               freq[s[r]] ++;
+             }
+            else    //freq[s[r+1]] == 1
+            {
+               freq[s[l]] --;
+               l++;
+            }
+
+            res = max(res, r - l + 1);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+
+    cout << Solution().lengthOfLongestSubstring( "abcabcbb" )<<endl; //3
+    cout << Solution().lengthOfLongestSubstring( "bbbbb" )<<endl;    //1
+    cout << Solution().lengthOfLongestSubstring( "pwwkew" )<<endl;   //3
+    cout << Solution().lengthOfLongestSubstring( "c" )<<endl;        //1
+    cout << Solution().lengthOfLongestSubstring( "" )<<endl;         //0
+
+    return 0;
+}
