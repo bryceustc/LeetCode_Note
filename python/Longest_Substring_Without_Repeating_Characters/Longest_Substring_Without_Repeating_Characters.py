d168a061e07a5777b4184b6ae2dfff34a2f4962c
Bryce <33057792+Everglowww@users.noreply.github.com>
2019-10-11 10:02:23 +0800

    Update Longest_Substring_Without_Repeating_Characters.py

--- python/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.py
+++ python/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.py
@@ -1,19 +1,23 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         freq = {}
         for i in range(256):
             freq[i] = 0
         ls = len(s)
-        i = max_len = 0
-        for j in range(ls):
+        max_len = 0
+        l=0
+        for r in range(ls):
             # Note that when charMap[ord(s[j])] >= i, it means that there are
             # duplicate character in current i,j. So we need to update i.
-            if freq[ord(s[j])] == 0:
-                freq(ord(s[j]) += 1
-            else
-            {
-                freq(ord(s[i])) -= 1
-                i +=1
-            }
-            max_len = max(max_len, j - i + 1)
+            
+            if freq[ord(s[r])] ==0:
+                freq[ord(s[r])] += 1
+            else:
+                freq[ord(s[l])] -= 1
+                l +=1
+            max_len = max(max_len, r - l )
         return max_len
