e248dfd7f026e7103be16065de84bfed0c563998
Bryce <33057792+Everglowww@users.noreply.github.com>
2019-10-11 10:13:34 +0800

    Update Longest_Substring_Without_Repeating_Characters.py

--- python/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.py
+++ python/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.py
@@ -8,15 +8,14 @@ class Solution(object):
         for i in range(256):
             freq[i] = 0
         ls = len(s)
-        max_len = 0
-        l=0
-        r=0
-        while(r < ls):
-            if freq[ord(s[r])] ==0:
-                freq[ord(s[r])] += 1
+        l=max_len = 0
+        r=-1
+        while(r+1 < len(s)):
+            if freq[ord(s[r+1])] ==0:
+                freq[ord(s[r+1])] += 1
                 r+=1
             else:
                 freq[ord(s[l])] -= 1
                 l +=1
-            max_len = max(max_len, r - l )
+            max_len = max(max_len, r - l +1 )
         return max_len
