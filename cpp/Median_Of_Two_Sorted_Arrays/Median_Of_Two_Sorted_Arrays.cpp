2a76bd5c96be2a07d3b128a98188b0df734a39dc
bryce <33057792+bryceustc@users.noreply.github.com>
2019-10-21 20:40:52 +0800

    Update Median_Of_Two_Sorted_Arrays.cpp

--- cpp/Median_Of_Two_Sorted_Arrays/Median_Of_Two_Sorted_Arrays.cpp
+++ cpp/Median_Of_Two_Sorted_Arrays/Median_Of_Two_Sorted_Arrays.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> v(nums1.begin(), nums1.end());
+    for (auto& e : nums2) {
+        v.push_back(e);
+    }
+    sort(v.begin(), v.end());
+    float n1;
+    if (v.size() % 2 == 0) {
+        //int i = v.size() / 2;
+        int j = v[v.size() / 2];
+        int k = v[v.size() / 2-1];
+        n1 = (j+k) / 2.0;
+    }
+    else {
+        n1 = v[v.size() / 2];
+    }
+    return n1;
+    }
 
+  };
