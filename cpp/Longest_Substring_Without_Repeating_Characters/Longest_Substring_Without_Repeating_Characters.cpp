847a622cbae8ed12b20b2dc85e786fd2313c0e6f
bryce <33057792+bryceustc@users.noreply.github.com>
2019-10-21 22:21:27 +0800

    Update Longest_Substring_Without_Repeating_Characters.cpp

--- cpp/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.cpp
+++ cpp/Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.cpp
@@ -41,6 +41,21 @@ public:
     }
 };
 
+class Solution2 {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> hash;
+        int res = 0;
+        for (int i = 0, j = 0; j < s.size(); j ++ )
+        {
+            hash[s[j]] ++ ;
+            while (hash[s[j]] > 1) hash[s[i ++ ]] -- ;
+            res = max(res, j - i + 1);
+        }
+        return res;
+    }
+};
+
 int main() {
 
     cout << Solution().lengthOfLongestSubstring( "abcabcbb" )<<endl; //3
