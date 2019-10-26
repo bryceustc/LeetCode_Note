c9fa4ee57adbe59208e0866ca698792ba4b52d99
bryce <33057792+bryceustc@users.noreply.github.com>
2019-10-26 15:37:27 +0800

    Update Longest-Palindromic-Substring.cpp

--- cpp/Longest-Palindromic-Substring/Longest-Palindromic-Substring.cpp
+++ cpp/Longest-Palindromic-Substring/Longest-Palindromic-Substring.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.size()<2) return s;
+        int n = s.size(), maxlen = 0, start = 0;
+        for (int i = 0; i < n - 1; i++ )
+        {
+            findPalindrome(s, i, i, start, maxlen);
+            findPalindrome(s, i, i+1, start, maxlen);
+        }
+        return s.substr(start, maxlen);
+    }
+    
+    void findPalindrome(string s, int left, int right, int& start, int& maxlen){
+        while(left >=0 && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        if (maxlen < right - left - 1){
+            start = left + 1;
+            maxlen = right - left - 1;
+        }
+    }
+};
