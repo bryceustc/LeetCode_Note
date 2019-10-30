95f31827dce78f356fd13a5f36eaa9caff26923b
bryce <33057792+bryceustc@users.noreply.github.com>
2019-10-30 15:44:38 +0800

    Update String-To-Integer-Atoi.cpp

--- cpp/String-To-Integer-Atoi/String-To-Integer-Atoi.cpp
+++ cpp/String-To-Integer-Atoi/String-To-Integer-Atoi.cpp
@@ -1,1 +1,56 @@
+/// Source : https://leetcode-cn.com/problems/string-to-integer-atoi/
+/// Author : bryce
+/// Time   : 2019-10-30
+#include <iostream>
+#include <string>
 
+class Solution {
+public:
+    int myAtoi(string str) {
+        int res = 0;
+        int i = 0;
+        int flag = 1;
+        // 1. 检查空格和回车
+        while (i < str.size() && (str[i] == ' ' || str[i] == '\t')) 
+        { 
+            i++; 
+        }
+        if (i >str.size()) 
+            return 0;
+        // 2. 检查符号
+        if (str[i] == '-') 
+        { 
+            flag = -1;
+            i++;
+        }
+        if (str[i] == '+') 
+        { 
+            if (flag == -1)
+                return 0;
+            else 
+                i++;
+        }
+        // 3. 计算数字
+        while (str[i]>='0' && str[i] <='9')
+        {
+            int r = str[i] - '0';
+            if ((res > INT_MAX /10 || (res == INT_MAX /10 && r > 7)))//比较最后一位 /是去掉最后一位，&是取最后一位
+            {
+                if (flag > 0)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
+            res = res*10 + r;
+            i++;
+        }
+        
+        res *= flag;
+        return res;
+    }
+};
+int main() {
+
+    cout << Solution().myAtoi( "123" )<<endl;
+    return 0;
+}
