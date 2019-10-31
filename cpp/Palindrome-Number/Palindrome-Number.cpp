7aab3dde0b6126d7cce35374fd37b6d40df06efe
bryce <33057792+bryceustc@users.noreply.github.com>
2019-10-31 09:28:47 +0800

    Update Palindrome-Number.cpp

--- cpp/Palindrome-Number/Palindrome-Number.cpp
+++ cpp/Palindrome-Number/Palindrome-Number.cpp
@@ -1,3 +1,39 @@
 /// Source : https://leetcode-cn.com/problems/palindrome-number/
 /// Author : bryce
 /// Time   : 2019-10-31
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int a = x;
+        int n = 0;
+        if (a < 0)
+            return false;
+        while (a != 0)
+        {
+            int m = a % 10;
+            a/=10;
+            if (n < INT_MAX/10 || (n == INT_MAX/10 && m <= 7))
+                n = n * 10 + m;
+            else 
+                return false;
+        }
+       return x == n;
+    }
+};
+
+class Solution2 {
+public:
+    bool isPalindrome(int x) {
+        int a = x;
+        long long n = 0;
+        if (a < 0)
+            return false;
+        while (a != 0)
+        {
+            int m = a % 10;
+            a/=10;
+            n = n * 10 + m;
+        }
+        return x == n;
+    }
+};
