e299c900e1e0ccae33f90eef01df38bf57c52c95
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-01 11:21:47 +0800

    Update Regular-Expression-Matching.cpp

--- cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
+++ cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
@@ -1,3 +1,25 @@
 /// Source : https://leetcode-cn.com/problems/regular-expression-matching/
 /// Author : bryce
 /// Time   : 2019-11-1
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size(), m = p.size();
+        vector<vector<bool>> f(n + 1, vector<bool>(m + 1, false));
+        s = " " + s;
+        p = " " + p;
+        f[0][0] = true;
+        for (int i = 0; i <= n; i++)
+            for (int j = 1; j <= m; j++) {
+                if (i > 0 && (s[i] == p[j] || p[j] == '.'))
+                    f[i][j] = f[i][j] | f[i - 1][j - 1];
+                if (p[j] == '*') {
+                    if (j >= 2)
+                        f[i][j] = f[i][j] | f[i][j - 2];
+                    if (i > 0 && (s[i] == p[j - 1] || p[j - 1] == '.'))
+                        f[i][j] = f[i][j] | f[i - 1][j];
+                }
+            }
+        return f[n][m];
+    }
+};
