8058014e30fdbb3d4d62ddee246f1e5d485c1c2a
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-02 21:00:36 +0800

    Update Regular-Expression-Matching.cpp

--- cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
+++ cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
@@ -23,3 +23,22 @@ public:
         return f[n][m];
     }
 };
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+        if (p.size() == 1) {
+            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
+        }
+        if (p[1] != '*') {
+            if (s.empty()) return false;
+            return (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        }
+        while (!s.empty() && (s[0] == p[0] || p[0] == '.')) {
+            if (isMatch(s, p.substr(2))) return true;
+            s = s.substr(1);
+        }
+        return isMatch(s, p.substr(2));
+    }
+};
