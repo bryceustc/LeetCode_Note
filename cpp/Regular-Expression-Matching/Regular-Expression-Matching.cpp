dd517237581a70e658937fd1e97a95a426acfb6a
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-02 22:31:08 +0800

    Update Regular-Expression-Matching.cpp

--- cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
+++ cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
@@ -1,6 +1,28 @@
 /// Source : https://leetcode-cn.com/problems/regular-expression-matching/
 /// Author : bryce
 /// Time   : 2019-11-1
+
+// Solution 1:
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();//如果为空，则返回 true 值。
+        if (p.size() == 1) {
+            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
+        }
+        if (p[1] != '*') {
+            if (s.empty()) return false;
+            return (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        }
+        while (!s.empty() && (s[0] == p[0] || p[0] == '.')) {
+            if (isMatch(s, p.substr(2))) return true;
+            s = s.substr(1);
+        }
+        return isMatch(s, p.substr(2));
+    }
+};
+
+// Solution2
 class Solution {
 public:
     bool isMatch(string s, string p) {
@@ -21,24 +43,5 @@ public:
                 }
             }
         return f[n][m];
-    }
-};
-
-class Solution {
-public:
-    bool isMatch(string s, string p) {
-        if (p.empty()) return s.empty();
-        if (p.size() == 1) {
-            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
-        }
-        if (p[1] != '*') {
-            if (s.empty()) return false;
-            return (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
-        }
-        while (!s.empty() && (s[0] == p[0] || p[0] == '.')) {
-            if (isMatch(s, p.substr(2))) return true;
-            s = s.substr(1);
-        }
-        return isMatch(s, p.substr(2));
     }
 };
