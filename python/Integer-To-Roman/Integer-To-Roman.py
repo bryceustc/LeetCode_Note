40b1e199a17150a6157b06d84344d54e067f2593
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-03 17:43:26 +0800

    Update Integer-To-Roman.py

--- python/Integer-To-Roman/Integer-To-Roman.py
+++ python/Integer-To-Roman/Integer-To-Roman.py
@@ -2,6 +2,8 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-11-03
 # Author: Bryce
+
+#Python Solution 1:
 class Solution(object):
     def intToRoman(self, num):
         """
@@ -15,4 +17,27 @@ class Solution(object):
             while num >= val[i] :
                 num -= val[i]
                 res += roman[i]
+        return res
+    
+    
+#Python Solution 2:
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        res = 0
+        if len(height) < 2:
+            return 0
+        i = 0
+        j = len(height) -1
+        while i < j:
+            width = j - i
+            high = min (height[i],height[j])
+            res =  max(res, width * high)
+            if height[i] > height[j]:
+                j -=1
+            else:
+                i +=1
         return res
