bf2c0d7eb9c05e39d9bb0955f0dfb738c772870e
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-03 16:50:19 +0800

    Update Integer-To-Roman.cpp

--- cpp/Integer-To-Roman/Integer-To-Roman.cpp
+++ cpp/Integer-To-Roman/Integer-To-Roman.cpp
@@ -163,7 +163,8 @@ public:
         string res;
         vector<int> val ={1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         vector<string> str = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        for (int i = 0; i < val.size(); i++) {
+        for (int i = 0; i < val.size(); i++) //类似贪心
+        {
             while (num >= val[i]) 
             {
                 num -= val[i];
