526919f37868f888333dd9dd95814285e3be28e4
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-03 17:30:06 +0800

    Update Container-With-Most-Water.cpp

--- cpp/Container-With-Most-Water/Container-With-Most-Water.cpp
+++ cpp/Container-With-Most-Water/Container-With-Most-Water.cpp
@@ -1,1 +1,27 @@
+/// Source : https://leetcode-cn.com/problems/container-with-most-water/
+/// Author : bryce
+/// Time   : 2019-11-2
 
+
+//C++ Solution 1:
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int res = 0;
+        int i, j = 0;
+        if (n < 2) return 0;
+        for (i = 0; i < n; i++)
+        {
+            for(j = i; j < n; j++)
+            {
+                int width = j - i;
+                int high = min(height[i],height[j]);
+                res = max(high * width, res);
+            }
+        }
+        return res;
+    }
+};
+
+// C++ Solution 2:
