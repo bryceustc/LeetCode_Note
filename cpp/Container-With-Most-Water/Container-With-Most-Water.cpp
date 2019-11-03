6a543b680dca995f0d450d5515eb84842dce7c3e
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-03 17:40:03 +0800

    Update Container-With-Most-Water.cpp

--- cpp/Container-With-Most-Water/Container-With-Most-Water.cpp
+++ cpp/Container-With-Most-Water/Container-With-Most-Water.cpp
@@ -2,7 +2,10 @@
 /// Author : bryce
 /// Time   : 2019-11-2
 
-
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
 //C++ Solution 1:
 class Solution {
 public:
@@ -25,3 +28,29 @@ public:
 };
 
 // C++ Solution 2:
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int res = 0;
+        int i, j = 0;
+        if (n < 2) return 0;
+        for (i = 0, j = n-1; i < j;)
+        {
+            int width = j - i;
+            int high = min(height[i],height[j]);
+            res = max(high * width, res);
+            if (height[i] > height[j])
+                j--;
+            else
+                i++;
+        }
+        return res;
+    }
+};
+
+int main() {
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    cout << Solution().maxArea(height) << endl; // false
+    return 0;
+}
