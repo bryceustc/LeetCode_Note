3d50a9ac131077b5a75f7e20c6eb3affca1b01a1
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-03 17:02:31 +0800

    Update Integer-To-Roman.py

--- python/Integer-To-Roman/Integer-To-Roman.py
+++ python/Integer-To-Roman/Integer-To-Roman.py
@@ -1,1 +1,18 @@
-
+#! /usr/bin/env python
+#! -*- coding=utf-8 -*-
+# Date: 2019-11-03
+# Author: Bryce
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        res = ''
+        val= [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        roman = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        for i in range(0,len(roman)):
+            while num >= val[i] :
+                num -= val[i]
+                res += roman[i]
+        return res
