c2413682846cc96b9cae9a831e4f7bf5d9eaf636
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-04 11:51:55 +0800

    Update Roman-To-Integer.py

--- python/Roman-To-Integer/Roman-To-Integer.py
+++ python/Roman-To-Integer/Roman-To-Integer.py
@@ -2,6 +2,7 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-11-04
 # Author: Bryce
+#Python Solution 1:
 class Solution(object):
     def romanToInt(self, s):
         """
@@ -24,3 +25,23 @@ class Solution(object):
                 res += words[s[i]]
                 i += 1
         return res
+
+    #Python Solution 2:
+    class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        words = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        res = 0
+        for i in range(len(s)):
+            if i < len(s)-1 and words[s[i+1]] > words[s[i]]:
+                res -= words[s[i]]
+            else:
+                res += words[s[i]]
+        return res
+    
+    if __name__ == " __main__ ":
+        a = "III"
+        print ( Solution().romanToInt (a) )
