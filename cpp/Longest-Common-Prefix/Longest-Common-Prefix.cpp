c1845cb7805387a496d70ab198138cb22b2eb9a2
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-05 13:51:34 +0800

    Update Longest-Common-Prefix.cpp

--- cpp/Longest-Common-Prefix/Longest-Common-Prefix.cpp
+++ cpp/Longest-Common-Prefix/Longest-Common-Prefix.cpp
@@ -2,6 +2,8 @@
 /// Source : https://leetcode-cn.com/problems/longest-common-prefix/
 /// Author : bryce
 /// Time   : 2019-11-05
+
+//C++ Solution 1:
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
@@ -9,23 +11,43 @@ public:
         int i = 0,b = 0;
         if (n == 0) return "";
         string res = "";
-        int a = strs[0].size();
+        int min_length = strs[0].size();
         for (i = 0; i < n; i++)
         {
             int m = strs[i].size();
-            a = min(a,m);
-            if (a >= m) b = i;
+            min_length = min(min_length,m);
+            if (min_length >= m) b = i;
         }
-        if (a==0) return "";
+        if (min_length==0) return "";
         res = strs[b];
         for (int l = 0; l < n; l++)
         {
-            for(int j = 0; j < a;j++)
+            for(int j = 0; j < min_length;j++)
             {
                 if (res[j] != strs[l][j])
                 res = res.substr(0,j);           
             }
         }
         return res;
+    }
+};
+
+// C++ Solution 2:
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0)
+            return "";
+        int min_length = strs[0].length();
+        for (int i = 1; i < strs.size(); i++)
+            min_length = min(min_length, int(strs[i].length()));
+
+        for (int s = 1; s <= min_length; s++) {
+            char c = strs[0][s - 1];
+            for (int i = 1; i < strs.size(); i++)
+                if (strs[i][s - 1] != c)
+                    return strs[0].substr(0, s - 1);
+        }
+        return strs[0].substr(0, min_length);
     }
 };
