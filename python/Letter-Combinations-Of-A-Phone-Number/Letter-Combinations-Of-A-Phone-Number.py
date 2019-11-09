0bafc689e91ef42487d7a317ec2d390f437d0451
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-09 16:12:25 +0800

    Update Letter-Combinations-Of-A-Phone-Number.py

--- python/Letter-Combinations-Of-A-Phone-Number/Letter-Combinations-Of-A-Phone-Number.py
+++ python/Letter-Combinations-Of-A-Phone-Number/Letter-Combinations-Of-A-Phone-Number.py
@@ -2,3 +2,61 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-11-08
 # Author: Bryce
+
+
+### Python Solution1:
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        动态规划
+        dp[i]: 前i个字母的所有组合
+        由于dp[i]只与dp[i-1]有关,可以使用变量代替列表存储降低空间复杂度
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        d = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+             '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        n = len(digits)
+        dp = [[] for _ in range(n)]
+        dp[0] = [x for x in d[digits[0]]]
+        for i in range(1, n):
+            dp[i] = [x + y for x in dp[i - 1] for y in d[digits[i]]]
+        return dp[-1]
+
+      
+### Python Solution2:
+class Solution(object):    
+    def letterCombinations(self, digits):
+        """
+        使用变量代替上面的列表
+        降低空间复杂度
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+             '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        n = len(digits)
+        res = ['']
+        for i in range(n):
+            res = [x + y for x in res for y in dic[digits[i]]]
+        return res
+
+      
+###  Python Solution3:
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        dic = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+             '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        if not digits:
+            return []
+        if len(digits) == 1:
+            return [x for x in dic[digits[0]]]
+        return [x + y for x in dic[digits[0]] for y in self.letterCombinations(digits[1:])]
