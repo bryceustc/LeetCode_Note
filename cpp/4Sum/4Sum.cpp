76112939bf5bd4c7a35e853b506b11a561ca4ce6
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-09 17:13:03 +0800

    Create 4Sum.cpp

---
+++ cpp/4Sum/4Sum.cpp
@@ -0,0 +1,8 @@
+/// Source : https://leetcode-cn.com/problems/4sum/
+/// Author : bryce
+/// Time   : 2019-11-09
+
+/*
+用迭代来求解，遍历digits中所有的数字，建立一个临时的字符串数组temp,然后通过数字到dict中取出字符串str，然后遍历取出字符串所有字符，
+再遍历当前结果res中的每一个字符串，将字符加到后面，并加入到临时字符串数组temp中。取出的字符串str遍历完成后，将临时字符串数组赋值给结果res
+*/
