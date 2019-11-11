fc279fcfead7818b1f72539fabdc8b53ff90b99f
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-11 16:14:24 +0800

    Update Valid-Parentheses.py

--- python/Valid-Parentheses/Valid-Parentheses.py
+++ python/Valid-Parentheses/Valid-Parentheses.py
@@ -2,3 +2,34 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-11-11
 # Author: Bryce
+
+### Python Solution 1:
+class Solution:
+    def isValid(self, s):
+        while '{}' in s or '()' in s or '[]' in s:
+            s = s.replace('{}', '')
+            s = s.replace('[]', '')
+            s = s.replace('()', '')
+        return s == ''
+
+      
+### Python Solution 2:
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(s) & 1 == 1:  # 位运算判断奇偶
+            return False
+        stack = []
+        hash_map = {'(': ')', '[': ']', '{': '}'}
+        for c in s:
+            if c in hash_map:
+                stack.append(c)
+                continue
+            elif stack and hash_map[stack[-1]] == c:
+                stack.pop()
+            else:
+                return False
+        return not stack
