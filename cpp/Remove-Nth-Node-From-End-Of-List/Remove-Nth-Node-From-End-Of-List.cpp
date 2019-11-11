16f8a4d1e37cd0db76ffd5146d4658bab4a3ed97
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-11 20:23:46 +0800

    Update Remove-Nth-Node-From-End-Of-List.cpp

--- cpp/Remove-Nth-Node-From-End-Of-List/Remove-Nth-Node-From-End-Of-List.cpp
+++ cpp/Remove-Nth-Node-From-End-Of-List/Remove-Nth-Node-From-End-Of-List.cpp
@@ -41,12 +41,37 @@ public:
 /*
 思路:
 (II)(一次遍历)
-1.第一次遍历求出链表长度。
-2.第二次遍历删掉指定结点。
-3.注意删除头结点的特殊情况。
+    我们可以设想假设设定了双指针p和q的话，当q指向末尾的NULL，p与q之间相隔的元素个数为n时，
+那么删除掉p的下一个指针就完成了要求。
+
+1.设置虚拟节点dummyHead指向head。
+2.移动q，直到p与q之间相隔的元素个数为n。
+3.同时移动p与q，直到q指向的为NULL，将p的下一个节点指向下下个节点。
 
 时间复杂度:遍历两次链表，故空间复杂度为 O(L)。
 空间复杂度:仅需要定义常数个指针变量，故空间复杂度为 O(1)
 */
 
 // C++ Solution 2:
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummyHead = new ListNode(0);   // 设置保护节点了删除头结点的时候能和删除其他结点一样处理，不用特殊处理头结点
+        dummyHead -> next = head;
+        ListNode* p = dummyHead;
+        ListNode* q = dummyHead;
+        for (int i = 0; i<=n;i++)
+        {
+            q = q -> next;
+        }
+        while (q != NULL)
+        {
+            p = p -> next;
+            q = q -> next;
+        }
+        p -> next = p -> next -> next;
+        ListNode* res = dummyHead -> next;
+        delete dummyHead;
+        return res;
+    }
+};
