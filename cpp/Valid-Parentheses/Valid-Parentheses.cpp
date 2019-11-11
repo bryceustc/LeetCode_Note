f368e149bd334f6745040139efcfb1acfa9536b0
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-11 15:19:03 +0800

    Update Valid-Parentheses.cpp

--- cpp/Valid-Parentheses/Valid-Parentheses.cpp
+++ cpp/Valid-Parentheses/Valid-Parentheses.cpp
@@ -4,7 +4,31 @@
 /// Time   : 2019-11-11
 
 /*
-LeetCode 中关于数字之类似的还有Two Sum ，3Sum ，3Sum Closest，解法的思路跟3Sum 基本没啥区别，就是多加了一层 for 循环，其他的都一样.
-使还有一种解法是为了避免重复项，STL 中的 TreeSet，其特点是不能有重复，如果新加入的数在 TreeSet 中原本就存在的话，插入操作就会失败，这
-样能很好的避免的重复项的存在。
+栈结构
+使用数据栈结构：
+1.遇到左括号，就进行压栈
+2.遇到右括号，如果此时栈为空，则直接返回false，如果不为空，就取出栈顶元素判断是否和当前括号匹配，若不匹配就返回false，否则匹配弹出栈顶
+3.最后判断栈是否为空，若为空则合法，否则不合法
 */
+class Solution {
+public:
+    bool isValid(string s) {
+      int n = s.size();
+      stack <char> stk;
+      if (n % 2 != 0) return false;  //判断元素个数是否为奇数，奇数直接返回false
+      for (int i = 0; i < n; i++)
+      {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+          stk.push(s[i]);
+        else
+        {
+          if (stk.empty()) return false;
+          if (s[i] == ')' && stk.top() != '(') return false;
+          if (s[i] == ']' && stk.top() != '[') return false;
+          if (s[i] == '}' && stk.top() != '{') return false;
+          stk.pop();   // pop 元素出栈
+        }
+      }
+      return (stk.empty());
+    }
+};
