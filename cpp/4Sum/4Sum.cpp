9ab96cd5b3e877723732d3c2491201fcbe944a72
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-11 10:33:05 +0800

    Update 4Sum.cpp

--- cpp/4Sum/4Sum.cpp
+++ cpp/4Sum/4Sum.cpp
@@ -3,49 +3,69 @@
 /// Time   : 2019-11-09
 
 /*
-用迭代来求解，遍历digits中所有的数字，建立一个临时的字符串数组temp,然后通过数字到dict中取出字符串str，然后遍历取出字符串所有字符，
-再遍历当前结果res中的每一个字符串，将字符加到后面，并加入到临时字符串数组temp中。取出的字符串str遍历完成后，将临时字符串数组赋值给结果res
+LeetCode 中关于数字之类似的还有Two Sum ，3Sum ，3Sum Closest，解法的思路跟3Sum 基本没啥区别，就是多加了一层 for 循环，其他的都一样.
+使还有一种解法是为了避免重复项，STL 中的 TreeSet，其特点是不能有重复，如果新加入的数在 TreeSet 中原本就存在的话，插入操作就会失败，这
+样能很好的避免的重复项的存在。
 */
+
+// C++ Solution 1 : 多一层for 循环
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> res;
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        for(int i = 0; i< n; i++)
+        if (n < 3) return res;
+        for (int i = 0;i < n-3;i++)
         {
-            while(i> 0 && i < n && nums[i] == nums[i-1])
+            if (i > 0 && nums[i] == nums[i-1]) continue;   //continue是结束单次循环，继续执行下一次循环
+            for (int j = i+1; j < n-2; j++)
             {
-                i++;
-            }
-
-            for (int j=i+1; j < n;j++)
-            {
-                while(j !=i+1 && j < n && nums[j] == nums[j-1])
+                if (j > i+1 && nums[j] == nums[j-1]) continue;
+                int l = j + 1; 
+                int r = n -1;
+                while (l<r)
                 {
-                    j++;
-                }
-                int l = j+1;
-                int r = n-1;
-                while(l < r)
-                {
-                    if (nums[i] + nums[j] + nums[l] + nums[r] == target)
+                    int sum = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum == target)
                     {
                         res.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        while (l < r && nums[l] == nums[l+1])
-                            ++l;
-                        while (l < r && nums[r] == nums[r-1])
-                            --r;
+                        while (l<r && nums[l] == nums[l-1]) ++l;
+                        while (l<r && nums[r] == nums[r+1]) --r;
+                        ++l;--r;
                     }
-                        
-                    else if (nums[i] + nums[j] + nums[l] + nums[r] > target)
-                        --r;
-                    else if (nums[i] + nums[j] + nums[l] + nums[r] < target)
-                        ++l;
+                    if (sum < target) ++l;
+                    if (sum > target) --r;
                 }
             }
         }
         return res;
             
+    }
+};
+
+
+//C++ Solution 2:  使用STL 中的 TreeSet
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int> &nums, int target) {
+        set<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < int(nums.size() - 3); ++i) {
+            for (int j = i + 1; j < int(nums.size() - 2); ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1, right = nums.size() - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        vector<int> out{nums[i], nums[j], nums[left], nums[right]};
+                        res.insert(out);
+                        ++left; --right;
+                    } else if (sum < target) ++left;
+                    else --right;
+                }
+            }
+        }
+        return vector<vector<int>>(res.begin(), res.end());
     }
 };
