d2cd043815f827412ad8cca484c19bf49ba7c375
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-11 20:55:18 +0800

    Update Remove-Nth-Node-From-End-Of-List.py

--- python/Remove-Nth-Node-From-End-Of-List/Remove-Nth-Node-From-End-Of-List.py
+++ python/Remove-Nth-Node-From-End-Of-List/Remove-Nth-Node-From-End-Of-List.py
@@ -12,6 +12,8 @@
 #         self.val = x
 #         self.next = None
 
+
+#### Python Solution 1: (一次遍历)
 class Solution(object):
     def removeNthFromEnd(self, head, n):
         """
@@ -31,3 +33,28 @@ class Solution(object):
         p.next = p.next.next
         res = dummyHead.next
         return res
+    
+    
+##### Python Solution 2：(二次遍历)
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        temp1 = head
+        temp2 = head
+        length = 0
+        m = 0
+        while temp1:
+            length +=1
+            temp1 = temp1.next
+        if n == length : return head.next
+        while temp2:
+            m+=1
+            if m == length - n:
+                temp2.next = temp2.next.next
+                break
+            temp2 = temp2.next
+        return head
