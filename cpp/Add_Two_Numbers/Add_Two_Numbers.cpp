cd2af1b476ec1c1e72fad621b61a56bb2eef1d65
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-12 22:27:14 +0800

    Update Add_Two_Numbers.cpp

--- cpp/Add_Two_Numbers/Add_Two_Numbers.cpp
+++ cpp/Add_Two_Numbers/Add_Two_Numbers.cpp
@@ -3,23 +3,32 @@
 /// Author : bryce
 /// Time   : 2019-10-13
 
+
+/*
+思路：
+
+1.从最低位至最高位，逐位相加，如果和大于等于10，则保留个位数字，同时向前一位进1.
+2.如果最高位有进位，则需在最前面补1.
+
+做有关链表的题目，有个常用技巧：添加一个虚拟头结点：ListNode *head = new ListNode(0);，可以简化边界情况的判断。
+
+时间复杂度：由于总共扫描一遍，所以时间复杂度是 O(n).
+空间复杂度：O(n)
+
+*/
 #include <iostream>
 
 using namespace std;
 
-/// Definition for singly-linked list.
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
-/// Create new LinkedList for result
-/// Time Complexity: O(n)
-/// Space Complexity: O(n)
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *sum = new ListNode(0);  //新建头结点，value为0
+        ListNode *sum = new ListNode(0);  //新建头结点，value为0，简化边界情况
         ListNode *l3=sum;
         int carry = 0, val1 = 0, val2 = 0, sum_val = 0;    //carry用于存储进位，sum_val存储相加                   
         while(l1!=NULL||l2!=NULL)            //三种情况：都不为空链表 1、等长，2、不等长； 有一个为空链表；
@@ -45,8 +54,3 @@ public:
         
     }
 };
-int main()
-{   
-    
-    return 0;
-}
