fa7095f7bedbbc943a2b712f2aeaacc638e1a851
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-12 11:36:21 +0800

    Update Merge-Two-Sorted-Lists.py

--- python/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.py
+++ python/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.py
@@ -2,3 +2,49 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-11-12
 # Author: Bryce
+
+
+###Python Solution 1:
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummyHead = ListNode(0)
+        temp = dummyHead
+        while l1 and l2:
+            if l1.val <= l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        if l1:
+            temp.next = l1
+        else:
+            temp.next = l2
+        l3 = dummyHead.next
+        return l3
+      
+      
+### Python Solution 2:
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
