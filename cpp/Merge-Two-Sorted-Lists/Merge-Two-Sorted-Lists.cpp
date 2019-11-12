3e889034dbdee809a9402db37ecc97f7fe706518
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-12 11:03:26 +0800

    Update Merge-Two-Sorted-Lists.cpp

--- cpp/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.cpp
+++ cpp/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.cpp
@@ -4,7 +4,42 @@
 
 /*
 思路:
+1.新建保护节点dummyHead,设置temp指针指向dummyHead
+2.如果当前l1指针指向的节点的值val比l2指针指向节点值小，则令temp的next指针指向l1，并且向后移动l1,否则，temp的next指针指向l2，并且向后移动l2
+3.向后移动temp
+4.循环上述步骤直至l1或l2为NULL
+5.判断如果l1为空，则temp的next指针指向l2,否则指向l1
 
+
+时间复杂度：
+两个链表各遍历一次，所以时间复杂度为O(n)
 */
-
-// C++ Solution 1:
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+      ListNode* dummyHead = new ListNode(0);
+      ListNode* temp = dummyHead;
+      ListNode* l3;
+      while( l1!=NULL && l2!=NULL)
+      {
+        if (l1->val <= l2->val)
+        {
+          temp->next = l1;
+          l1 = l1->next;
+        }
+        else
+        {
+          temp->next = l2;
+          l2 = l2->next;
+        }
+        temp = temp->next;
+      }
+      if (l1!=NULL) 
+        temp->next = l1;
+      else
+        temp->next = l2;
+      l3 = dummyHead->next;
+      delete dummyHead;
+      return l3;
+    }
+};
