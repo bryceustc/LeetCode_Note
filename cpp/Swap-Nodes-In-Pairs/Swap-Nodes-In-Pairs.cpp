5d8fa6111c63100b927c33e0d8032c05f2183871
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-15 22:21:31 +0800

    Update Swap-Nodes-In-Pairs.cpp

--- cpp/Swap-Nodes-In-Pairs/Swap-Nodes-In-Pairs.cpp
+++ cpp/Swap-Nodes-In-Pairs/Swap-Nodes-In-Pairs.cpp
@@ -19,3 +19,18 @@ O(1)
 
 
 // C++ Solution 1:
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode *dummy = new ListNode(-1), *pre = dummy;
+        dummy->next = head;
+        while (pre->next && pre->next->next) {
+            ListNode *t = pre->next->next;
+            pre->next->next = t->next;
+            t->next = pre->next;
+            pre->next = t;
+            pre = t->next;
+        }
+        return dummy->next;
+    }
+};
