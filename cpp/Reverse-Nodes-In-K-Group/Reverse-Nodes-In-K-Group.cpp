90da1f07d2b8bd0badee81cd698d5c3fe0c62b63
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-16 21:50:24 +0800

    Update Reverse-Nodes-In-K-Group.cpp

--- cpp/Reverse-Nodes-In-K-Group/Reverse-Nodes-In-K-Group.cpp
+++ cpp/Reverse-Nodes-In-K-Group/Reverse-Nodes-In-K-Group.cpp
@@ -4,9 +4,79 @@
 
 /*
 思路：
+这道题让我们以每k个为一组来翻转链表，实际上是把原链表分成若干小段，然后分别对其进行翻转，那么肯定总共需要两个函数，一个是用来分段的，
+一个是用来翻转的，以题目中给的例子来看，对于给定链表 1->2->3->4->5，一般在处理链表问题时，大多时候都会在开头再加一个 dummy node，
+因为翻转链表时头结点可能会变化，为了记录当前最新的头结点的位置而引入的 dummy node，加入 dummy node 后的链表变为 0->1->2->3->4->5，如果k为3
+的话，目标是将 1,2,3 翻转一下，那么需要一些指针，pre 和 next 分别指向要翻转的链表的前后的位置，然后翻转后 pre 的位置更新到如下新的位置：
 
+#####################
+0->1->2->3->4->5
+|        |  |
+pre     cur next
+
+0->3->2->1->4->5
+   |     |  |
+  cur   pre next
+       (last)
+ 0->3->2->1->4->5
+          |  |  |
+         pre cur next       
+#####################
 时间复杂度：
-O(kn)
+遍历一遍链表，O(L)
 空间复杂度：
 O(1)
 */
+//   C++ Solution 1:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+      ListNode* dummyHead = new ListNode(0);
+      ListNode* pre = dummyHead;
+      dummyHead->next = head;
+      ListNode* cur = head;
+      ListNode* res;
+      if (head != NULL || k==1)
+      {
+        return head;
+      }
+      for (int i=1;cur!= NULL;++i)
+      {
+        if (i%k==0)
+        {
+          ListNode* next = cur->next;
+          pre = reverseOneGroup(pre, next);
+          cur = pre->next;
+        }
+        else
+        {
+          cur = cur->next;
+        }
+      }
+      res = dummyHead->next;
+      delete dummyHead;
+      return res;
+      }
+  
+    ListNode* reverseOneGroup(ListNode* pre, ListNode* next)
+    {
+      ListNode* last = pre->next;
+      ListNode* cur = last->next;
+      while(cur!=next)
+      {
+        last->next = cur->next;
+        cur->next = pre->next;
+        pre->next = cur;
+        cur = last->next;
+      }
+      return last;
+    }
+  };
