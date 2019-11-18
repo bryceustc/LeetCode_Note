ec1fa45bf70946b4b6815806b452b3faf22add49
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-18 14:11:48 +0800

    Update Remove-Duplicates-From-Sorted-Array.py

--- python/Remove-Duplicates-From-Sorted-Array/Remove-Duplicates-From-Sorted-Array.py
+++ python/Remove-Duplicates-From-Sorted-Array/Remove-Duplicates-From-Sorted-Array.py
@@ -2,3 +2,46 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-11-17
 # Author: Bryce
+
+######Python Solution 1:
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        n = len(nums)
+        if n==0:
+            return res
+        k = 0
+        for i in range(1,n):
+            if nums[k] != nums[i]:
+                k+=1
+                nums[k] = nums[i]
+        res = k+1
+        return res
+   
+  
+######Python Solution 2:
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        n = len(nums)
+        if n==0:
+            return res
+        l = 0
+        r = 0
+        while r < n:
+            if nums[l] == nums[r]:
+                r+=1
+            else:
+                l+=1
+                nums[l] = nums[r]
+                r+=1
+        res = l+1
+        return res
