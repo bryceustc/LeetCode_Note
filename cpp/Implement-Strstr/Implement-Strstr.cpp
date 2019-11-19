8b86f0810a52fcac1a30a240bf7806c5bd13f970
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-19 11:26:55 +0800

    Update Implement-Strstr.cpp

--- cpp/Implement-Strstr/Implement-Strstr.cpp
+++ cpp/Implement-Strstr/Implement-Strstr.cpp
@@ -1,17 +1,64 @@
 /// Source : https://leetcode-cn.com/problems/implement-strstr/
 /// Author : bryce
-/// Time   : 2019-11-18
+/// Time   : 2019-11-19
 
 /*
-思路(双指针移动)：
-1. 如果nums为空，直接返回0
-2. 初始化k = 0,从i=1开始遍历整个数组，如果nums[k]不等于nums[i]，说明没有遇到重复元素，就是新元素，令nums[++k] = nums[i]
-3. 返回res = k+1
+思路(暴力枚举法)：
+1. 如果needle为空，直接返回0
+2. 如果haystack为空或者子串长度大于母串长度则返回-1
+3. 遍历haystack字符串，后对于每一个字符，都遍历一遍子字符串，一个一个字符的对应比较，如果对应位置有不等的，则跳出循环，
+  如果一直都没有跳出循环，则说明子字符串出现了，则返回起始位置即i.
+4. 跳出循环则返回-1
 
 时间复杂度：
-遍历一遍数组，时间复杂度为O(n)
+暴力枚举O(nm)
 
-空间复杂度：
-仅定义了两个临时变量，故空间复杂度是O(1)。
 */
 // C++ Solution 1:
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+      int m = needle.size();
+      int n = haystack.size();
+      if (needle.empty()) return 0;
+      if (haystack.empty() || m > n) return -1;
+      for (int i = 0; i < n; i++)
+      {
+        int j = 0;
+        int k = i;
+        while (haystack[k] == needle[j] && j<m)
+        {
+          j++;
+          k++;
+        }
+        if (j==m) return i;
+      }
+      return -1;
+    }
+};
+
+// C++ Solution 2:
+// 其实可以不用遍历整个母字符串，而是遍历到剩下的长度和子字符串相等的位置即可，这样可以提高运算效率
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+      int m = needle.size();
+      int n = haystack.size();
+      if (needle.empty()) return 0;
+      if (haystack.empty() || m > n) return -1;
+      for (int i =0; i < n-m+1;i++)
+      {
+        int j = 0;
+        int k = i;
+        while (haystack[k] == needle[j] && j < m)
+        {
+          j++;
+          k++;
+        }
+        if (j==m) return i;
+      }
+      return -1;
+    }
+};
+
+//C++ Solution 3: (KMP算法)
