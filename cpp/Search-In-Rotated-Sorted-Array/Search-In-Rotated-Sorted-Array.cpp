fdc69eb0fcbf596b56d55e7ed9aff13e7608abb0
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-26 15:26:24 +0800

    Update Search-In-Rotated-Sorted-Array.cpp

--- cpp/Search-In-Rotated-Sorted-Array/Search-In-Rotated-Sorted-Array.cpp
+++ cpp/Search-In-Rotated-Sorted-Array/Search-In-Rotated-Sorted-Array.cpp
@@ -32,5 +32,36 @@ O(1)。
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+      int n = nums.size();
+      int start = 0;
+      int end = n-1;
+      while (end >= start)
+      {
+        int mid = start + (end-start)/2;
+        if (nums[mid] == target) return mid;
+        if (nums[mid] < nums[end])
+        {
+          if (nums[mid]<target && nums[end] >= target)
+          {
+            start = mid +1;
+          }
+          else
+          {
+            end = mid - 1;
+          }
+        }
+        else
+        {
+          if (nums[start]<=target && nums[mid]>target)
+          {
+            end = mid - 1;
+          }
+          else
+          {
+            start = mid + 1;
+          }
+        }
+      }
+      return -1;
     }
-    };
+};
