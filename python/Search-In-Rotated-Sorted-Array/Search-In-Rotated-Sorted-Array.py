6ebbd25163fc9748574589243d78be25b0b8893f
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-26 16:09:50 +0800

    Update Search-In-Rotated-Sorted-Array.py

--- python/Search-In-Rotated-Sorted-Array/Search-In-Rotated-Sorted-Array.py
+++ python/Search-In-Rotated-Sorted-Array/Search-In-Rotated-Sorted-Array.py
@@ -3,6 +3,8 @@
 # Date: 2019-11-24
 # Author: Bryce
 
+
+######Python Solution 1: 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         n = len(nums)
@@ -25,3 +27,27 @@ class Solution:
                 else:
                     start = mid+1;
         return -1;
+
+######Python Solution 2:    
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        if n==0 : return -1
+        start = 0
+        end = n - 1
+        while end>=start:
+            mid = start + (end - start)//2
+            #mid = int(mid)
+            if nums[mid]==target:
+                return mid
+            if nums[mid]>=nums[start]:
+                if nums[start]<=target and nums[mid] > target:
+                    end = mid - 1
+                else:
+                    start = mid+1
+            else:
+                if nums[mid]<target and nums[end]>= target:
+                    start = mid +1
+                else:
+                    end = mid - 1
+        return -1
