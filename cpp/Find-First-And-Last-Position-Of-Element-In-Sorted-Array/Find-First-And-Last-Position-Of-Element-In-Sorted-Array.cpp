1a5e87438017bd5dbeeec8b6f944b529afcb80eb
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-29 10:37:41 +0800

    Update Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp

--- cpp/Find-First-And-Last-Position-Of-Element-In-Sorted-Array/Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp
+++ cpp/Find-First-And-Last-Position-Of-Element-In-Sorted-Array/Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp
@@ -7,3 +7,48 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
+      vector<int> res = {-1,-1};
+      int n = nums.size();
+      if (nums.empty()) return res;
+      int start = 0;
+      int end = n;
+      while (end > start)   // 寻找左边界，搜索区间为左闭右开
+      {
+        int mid = start + (end - start)/2;
+        if (nums[mid]==target)
+        {
+            end = mid;
+        }
+        if (nums[mid]<target)
+        {
+            start = mid + 1;
+        }
+        if (nums[mid]>target)
+        {
+            end = mid;
+        }
+      }
+      if (start==n || nums[start]!=target) return res;
+      res[0] = start;
+      start = 0;
+      end = n;
+      while(end > start) //寻找右边界，搜索区间为左开右闭
+      {
+        int mid = start + (end - start)/2;
+        if (nums[mid]==target) 
+        {
+            start = mid + 1;
+        }
+        if (nums[mid] < target)
+        {
+            start = mid + 1;
+        }
+        if (nums[mid] > target)
+        {
+            end = mid;
+        }
+      }
+      res[1] = end - 1;
+      return res;
+    }
+};
