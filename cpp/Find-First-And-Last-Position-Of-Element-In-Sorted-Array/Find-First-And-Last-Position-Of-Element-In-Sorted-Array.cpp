d915d567fad07ace9e5df90be76474822b882113
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-29 10:34:38 +0800

    Create Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp

---
+++ cpp/Find-First-And-Last-Position-Of-Element-In-Sorted-Array/Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp
@@ -0,0 +1,9 @@
+/// Source : https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+/// Author : bryce
+/// Time   : 2019-11-25
+
+
+// C++ Solution 1: 两次二分查找法
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
