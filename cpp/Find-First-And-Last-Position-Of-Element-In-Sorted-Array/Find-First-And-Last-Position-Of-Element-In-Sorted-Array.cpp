2ff2183cb2e93024167184bce0b9733883fa6d27
bryce <33057792+bryceustc@users.noreply.github.com>
2019-11-29 11:08:42 +0800

    Update Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp

--- cpp/Find-First-And-Last-Position-Of-Element-In-Sorted-Array/Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp
+++ cpp/Find-First-And-Last-Position-Of-Element-In-Sorted-Array/Find-First-And-Last-Position-Of-Element-In-Sorted-Array.cpp
@@ -4,6 +4,10 @@
 
 
 // C++ Solution 1: 两次二分查找法
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
@@ -49,6 +53,50 @@ public:
         }
       }
       res[1] = end - 1;
+      return res;
+    }
+};
+
+
+int main()
+{
+    vector<int> nums = {5,7,7,8,8,10};
+    int target = 8;
+    vector<int> res;
+    res=Solution().searchRange(nums,target);
+    int m = res.size();
+    for (int i = 0; i < m; i++)
+    {
+        cout << res[i]<< endl;
+    }
+    system("pause");
+    return 0;
+}
+
+/// C++ Solution 2: 线性扫描，两次遍历，寻找左右边界。
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+      vector<int> res = {-1,-1};
+      int n = nums.size();
+      if (nums.empty()) return res; 
+      for(int i=0;i<n;i++)
+      {
+          if(nums[i]==target)
+          {
+              res[0] = i;
+              break;
+          }
+      }
+      if (res[0]==-1) return res;
+      for (int j=n-1;j>=0 ;j--)
+      {
+          if (nums[j]==target) 
+          {
+              res[1] =j;
+              break;
+          }
+      }
       return res;
     }
 };
