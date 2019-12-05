3928d9f49954cdc5e631ad269779c32686ed272c
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-05 19:49:45 +0800

    Update Multiply-Strings.py

--- python/Multiply-Strings/Multiply-Strings.py
+++ python/Multiply-Strings/Multiply-Strings.py
@@ -2,3 +2,20 @@
 #! -*- coding=utf-8 -*-
 # Date: 2019-12-4
 # Author: Bryce
+
+
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        n1,n2 = len(num1),len(num2)
+        res = [0 for i in range(n1+n2)]
+        for i in range(n2-1,-1,-1):
+            for j in range(n1-1,-1,-1):
+                x,y = ord(num1[j])-ord('0'),ord(num2[i])-ord('0')
+                temp = x*y + res[i+j+1]
+                res[i+j+1] = temp % 10
+                res[i+j] += temp // 10
+        for i in range(0,n1+n2):
+            if res[i] != 0:
+                res = ''.join(str(x) for x in res[i:])
+                return res
+        return "0"
