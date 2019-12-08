ff69ca8074384de47c11cf806e6930ab098e6a78
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-08 16:33:31 +0800

    Update Rotate-Image.py

--- python/Rotate-Image/Rotate-Image.py
+++ python/Rotate-Image/Rotate-Image.py
@@ -1,3 +1,4 @@
+####### Solution 1:
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
@@ -21,3 +22,19 @@ class Solution:
                 matrix[i][k] = temp
                 j+=1
                 k-=1
+                
+                
+####### Solution 2:
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range (n):
+            for j in range(i,n-1-i):
+                temp = matrix[i][j]
+                matrix[i][j] = matrix[n-1-j][i]
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+                matrix[j][n-1-i] = temp
