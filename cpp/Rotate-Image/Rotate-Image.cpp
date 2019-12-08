249fdf30eda4e99c946791c0b8f718f12e550d24
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-08 16:29:42 +0800

    Update Rotate-Image.cpp

--- cpp/Rotate-Image/Rotate-Image.cpp
+++ cpp/Rotate-Image/Rotate-Image.cpp
@@ -1,4 +1,4 @@
-
+//Solution 1:
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
@@ -24,6 +24,25 @@ public:
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][k];
                 matrix[i][k] = temp;
+            }
+        }
+    }
+};
+
+//Solution 2:
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i=0;i<n/2;i++)
+        {
+            for (int j=i;j<n-1-i;j++)
+            {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
             }
         }
     }
