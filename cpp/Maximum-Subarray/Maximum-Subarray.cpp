77c7e0fa6e471b0e645d33dcb1fd3db85f679399
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-09 21:50:40 +0800

    Update Maximum-Subarray.cpp

--- cpp/Maximum-Subarray/Maximum-Subarray.cpp
+++ cpp/Maximum-Subarray/Maximum-Subarray.cpp
@@ -5,8 +5,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int> &nums){
-        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值
-        int res = INT_MIN;
+        //类似寻找最大最小值的题目，初始值一定要定义成理论上的最小最大值 
+        // 或者不用初始化为理论的最小值
+        //int res = INT_MIN;
+        int res = nums[0];
         int n = nums.size();
         for (int i=0;i<n;i++)
         {
