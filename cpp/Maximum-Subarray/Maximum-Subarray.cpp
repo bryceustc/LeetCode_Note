f682e5321f5b0e5f8a7ce2eb8abaf721145037cc
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-09 22:38:13 +0800

    Update Maximum-Subarray.cpp

--- cpp/Maximum-Subarray/Maximum-Subarray.cpp
+++ cpp/Maximum-Subarray/Maximum-Subarray.cpp
@@ -25,8 +25,44 @@ public:
 
 
 // Solution 2:
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums){
+        int res = nums[0];
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
+        for (int i=1;i<n;i++)
+        {
+            dp[i] = max(dp[i-1]+nums[i],nums[i]);
+            res = max(res,dp[i]);
+        }
+        return res;
+    }
+};
 
 
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums){
+        int res = nums[0];
+        int n = nums.size();
+        int sum = 0;
+        for (int i=0;i<n;i++)
+        {
+            if (sum>0)
+            {
+                sum+=nums[i];
+            }
+            else
+            {
+                sum = nums[i];
+            }
+            res = max(res,sum);
+        }
+        return res;
+    }
+};
 
 
 // Solution 3:
