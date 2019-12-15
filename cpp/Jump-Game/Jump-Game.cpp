189e52ec30a9d3940f495665afe60a2e65c8fde3
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-15 22:08:22 +0800

    Update Jump-Game.cpp

--- cpp/Jump-Game/Jump-Game.cpp
+++ cpp/Jump-Game/Jump-Game.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+bool canJump(vector<int>& nums) 
+{
+    int n=nums.size();
+    if (nums.empty())  return 0;
+    vector<int> dp(n,0);
+    for (int i=1;i<n;i++)
+    {
+        dp[i]=max(dp[i-1],nums[i-1])-1;
+        if (dp[i]<0) return 0;
+    }
+    return 1;
+}
+};
