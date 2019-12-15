8659f91845eb4b0fd279642efb73f2ba05c8fcef
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-15 13:51:15 +0800

    Update Spiral-Matrix.cpp

--- cpp/Spiral-Matrix/Spiral-Matrix.cpp
+++ cpp/Spiral-Matrix/Spiral-Matrix.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        if (matrix.empty()) return res;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int u = 0;
+        int d = m-1;
+        int l = 0;
+        int r = n-1;
+        while(true)
+        {
+          for (int i=l;i<=r;i++) res.push_back(matrix[u][i]);//向右移动至最右
+          if (++u > d) break;//重新定义上边界
+          for (int i=u;i<=d;i++) res.push_back(matrix[i][r]);//向下
+          if (--r < l) break;//重新定义右边界
+          for (int i=r;i>=l;i--) res.push_back(matrix[d][i]);//向左
+          if (--d < u) break;//重新定义下边界
+          for (int i=d;i>=u;i--) res.push_back(matrix[i][l]);//向上
+          if (++l > r) break;//重新定义左边界
+        }
+        return res;
+    }
+};
