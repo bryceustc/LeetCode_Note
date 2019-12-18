56703de6511fb6f6199093c7ae4f73217579e27d
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-18 21:55:16 +0800

    Update Length-Of-Last-Word.cpp

--- cpp/Length-Of-Last-Word/Length-Of-Last-Word.cpp
+++ cpp/Length-Of-Last-Word/Length-Of-Last-Word.cpp
@@ -1,1 +1,20 @@
-
+// C++ Solution 1:
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int res  = 0;
+         if (s.empty()) return res;
+        int n = s.size();
+        int m=n-1;
+        while(m>=0 && s[m]=='  ')
+        {
+            m--;
+        }
+        for (int i=m;i>=0;i--)
+        {
+            if (s[i]=='  ') break;
+            res+=1;
+        }
+        return res;
+    }
+};
