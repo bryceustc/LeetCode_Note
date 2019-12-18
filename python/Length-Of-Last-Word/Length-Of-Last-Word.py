1f0464bc4e20b518dd0e723a4ebcd074c1c3d0fe
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-18 21:54:54 +0800

    Update Length-Of-Last-Word.py

--- python/Length-Of-Last-Word/Length-Of-Last-Word.py
+++ python/Length-Of-Last-Word/Length-Of-Last-Word.py
@@ -1,1 +1,17 @@
+### Python Solution 1:
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        res =0
+        n=len(s)
+        if n==0:
+            return res
+        m = n-1
+        while m>=0 and s[m]==' ':
+            m-=1
+        for i in range (m,-1,-1):
+            if s[i]==' ':
+                break
+            res+=1
+        return res
 
+ ### Python Solution 2:
