753cba190ee86cba4fdb6e35ff3e8a8d62fdad6c
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-19 21:37:48 +0800

    Update Search-A-2D-Matrix-II.py

--- python/Search-A-2D-Matrix-II/Search-A-2D-Matrix-II.py
+++ python/Search-A-2D-Matrix-II/Search-A-2D-Matrix-II.py
@@ -1,1 +1,72 @@
+## Python Solution 1:
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m=len(matrix)
+        if m==0:
+            return False
+        n=len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+               if matrix[i][j]==target:
+                   return True
+        return False
+## Python Solution 2:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m=len(matrix)
+        if m==0:
+            return False
+        n=len(matrix[0])
+        for i in range(m):
+            start = 0
+            end = n-1
+            while end>=start:
+                mid = start + (end-start)//2
+                if matrix[i][mid] == target:
+                    return True
+                if matrix[i][mid]<target:
+                    start=mid+1
+                if matrix[i][mid]>target:
+                    end=mid-1
+        return False
 
+## Python Solution 3:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m=len(matrix)
+        if m==0:
+            return False
+        n=len(matrix[0])
+        i=0
+        j=n-1
+        while i<m and j>=0:
+            if matrix[i][j]==target:
+                return True
+            elif matrix[i][j]>target:
+                j-=1
+            else:
+                i+=1
+        return False
+
+## Python Solution 4:
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m=len(matrix)
+        if m==0:
+            return False
+        n=len(matrix[0])
+        i=m-1
+        j=0
+        while i>=0 and j<n:
+            if matrix[i][j]==target:
+                return True
+            elif matrix[i][j]>target:
+                i-=1
+            else:
+                j+=1
+        return False
