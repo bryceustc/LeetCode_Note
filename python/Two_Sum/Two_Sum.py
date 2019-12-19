a59e1bdfb04b3bc57ec6763a330574a83aa99029
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-19 11:22:11 +0800

    Update Two_Sum.py

--- python/Two_Sum/Two_Sum.py
+++ python/Two_Sum/Two_Sum.py
@@ -8,7 +8,7 @@ class Solution:
     dic={ };
     for index, num in enumerate (nums):
         if num in dic :
-            return  [dic[num]], index]
+            return  [dic[num], index]
         dic [target - num]= index
 if __name__ == " __main__ ":
     nums = [ 2 , 7 , 11 , 15 ]
