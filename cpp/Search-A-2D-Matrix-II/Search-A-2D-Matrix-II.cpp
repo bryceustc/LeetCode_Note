731dd2e3bc8b1ee938c1edaad6ee1f1542572059
bryce <33057792+bryceustc@users.noreply.github.com>
2019-12-19 21:45:15 +0800

    Update Search-A-2D-Matrix-II.cpp

--- cpp/Search-A-2D-Matrix-II/Search-A-2D-Matrix-II.cpp
+++ cpp/Search-A-2D-Matrix-II/Search-A-2D-Matrix-II.cpp
@@ -1,1 +1,84 @@
+// C++ Solution 1:
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            if (matrix.empty()) return false;
+            int m = matrix.size();
+            int n = matrix[0].size();
+            for (int i=0;i<m;i++)
+            {
+                for (int j=0;j<n;j++)
+                {
+                    if (matrix[i][j]==target)
+                        return true;
+                }
+            }
+            return false;        
+    }
+};
+// C++ Solution 2:
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+            if (matrix.empty()) return false;
+            int m = matrix.size();
+            int n = matrix[0].size();
+            for (int i=0;i<m;i++)
+            {
+                int start = 0;
+                int end = n-1;
+                while(end>=start)
+                {
+                    int mid = start+(end-start)/2;
+                    if (matrix[i][mid]==target)
+                        return true;
+                    else if (matrix[i][mid]<target)
+                        start=mid+1;
+                    else if (matrix[i][mid]>target)
+                        end=mid-1;
+                }
+            }
+            return false;        
+    }
+};
 
+// C++ Solution 3:
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty()) return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i=0,j=n-1;
+        while(i<m && j>=0)
+        {
+            if (matrix[i][j]==target)
+                return true;
+            if (matrix[i][j]>target)
+                j--;  //左移
+            else
+                i++; // 下移
+        }
+        return false;
+    }
+};
+// C++ Solution 4:
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty()) return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i=m-1,j=0;
+        while(i>=0&& j<n)
+        {
+            if (matrix[i][j]==target)
+                return true;
+            if (matrix[i][j]>target)
+                i--;  //上移
+            else
+                j++; // 右移
+        }
+        return false;
+    }
+};
