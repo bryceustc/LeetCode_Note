0073763812b671c066090e4482a1169d1ed5faed
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-04 17:26:10 +0800

    Update Maximum-Subarray.cpp

--- cpp/Maximum-Subarray/Maximum-Subarray.cpp
+++ cpp/Maximum-Subarray/Maximum-Subarray.cpp
@@ -115,3 +115,23 @@ public:
         return (leftSum + rightSum);
     }
 };
+
+
+/// C++ Solution 4:
+class Solution {
+public:
+    int maxSubArray(vector<int> nums) {
+        int res = nums[0];
+        int n = nums.size();
+        int sum = 0;
+        for (int i=0;i<n;i++)
+        {
+            if (sum > 0)
+                sum+=nums[i];
+            else
+                sum = nums[i];
+            res = max(res,sum);
+        }
+        return res;
+    }
+};
