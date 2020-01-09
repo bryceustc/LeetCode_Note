0410c7518d9ae375cb703fde8d6f715cce7e988f
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-09 11:27:46 +0800

    Update Ugly-Number.cpp

--- cpp/Ugly-Number/Ugly-Number.cpp
+++ cpp/Ugly-Number/Ugly-Number.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num<1)
+            return false;
+        while(num%5==0)
+        {
+            num/=5;
+        }
+        while(num%3==0)
+        {
+            num/=3;
+        }
+        while(num%2==0)
+        {
+            num/=2;
+        }
+        return num==1;
+    }
+};
