471a6d5bbf7886d81fc2a47576d69cd3e24445ae
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-09 11:28:53 +0800

    Update Ugly-Number-II.cpp

--- cpp/Ugly-Number-II/Ugly-Number-II.cpp
+++ cpp/Ugly-Number-II/Ugly-Number-II.cpp
@@ -1,1 +1,34 @@
-
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num<1)
+            return false;
+        while(num%5==0)
+        {
+            num/=5;
+        }
+        while(num%3==0)
+        {
+            num/=3;
+        }
+        while(num%2==0)
+        {
+            num/=2;
+        }
+        return num==1;
+    }
+    int nthUglyNumber(int n) {
+        int i = 0;
+        int num = 0;
+        while(i<n)
+        {
+            num++;
+            if (isUgly(num))
+            {
+                i++;
+            }
+            
+        }
+        return num;
+    }
+};
