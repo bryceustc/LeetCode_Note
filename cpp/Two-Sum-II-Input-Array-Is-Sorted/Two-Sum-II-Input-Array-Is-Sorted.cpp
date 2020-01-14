36ff054d424666c7a1c29e7635164c669d0accc9
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-14 21:26:59 +0800

    Update Two-Sum-II-Input-Array-Is-Sorted.cpp

--- cpp/Two-Sum-II-Input-Array-Is-Sorted/Two-Sum-II-Input-Array-Is-Sorted.cpp
+++ cpp/Two-Sum-II-Input-Array-Is-Sorted/Two-Sum-II-Input-Array-Is-Sorted.cpp
@@ -1,1 +1,76 @@
-
+// C++ Solution 1:\
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector <int> res (2,0);
+        if (numbers.empty())
+            return res;
+        int n = numbers.size();
+        for (int i=0;i<n;i++)
+        {
+            for (int j = i+1; j<n;j++)
+            {
+                if (numbers[i]+numbers[j]==target)
+                {
+                    res[0] = numbers[i];
+                    res[1] = numbers[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
+// C++ Solution 2:
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector <int> res (2,0);
+        if (numbers.empty())
+            return res;
+        int n = numbers.size();
+        unordered_map<int,int> record;
+        for (int i=0;i<n;i++)
+        {
+            int complement = target -numbers[i];
+            if (record.find(complement)!=record.end())
+            {
+                res[0] = complement;
+                res[1] = nums[i];
+                break;
+            }
+            record[numbers[i]] = i;
+        }
+        return res;
+    }
+};
+// C++ Solution 3:
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector <int> res (2,0);
+        if (nums.empty())
+            return res;
+        int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        while (right>left)
+        {
+            if (nums[left]+nums[right]==target)
+            {
+                res[0] = nums[left];
+                res[1] = nums[right];
+                break;
+            }
+            if (nums[left]+nums[right]>target)
+            {
+                right--;
+            }
+            if (nums[left]+nums[right]<target)
+            {
+                left++;
+            }
+        }
+        return res;
+    }
+};
