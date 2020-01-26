d9896f2601237e1ae0ef9239c52d7346aa5aff37
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-26 16:55:39 +0800

    Update Regular-Expression-Matching.cpp

--- cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
+++ cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
@@ -6,19 +6,22 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        if (p.empty()) return s.empty();//如果为空，则返回 true 值。
-        if (p.size() == 1) {
-            return (s.size() == 1 && (s[0] == p[0] || p[0] == '.'));
+        if (p.empty()) 
+            return s.empty();
+        if (p[1]!='*')
+        {
+            if (s[0]==p[0] || p[0]=='.' && !s.empty())
+                return isMatch(s.substr(1),p.substr(1));
+            else
+                return false;
         }
-        if (p[1] != '*') {
-            if (s.empty()) return false;
-            return (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        else
+        {
+            if (s[0]==p[0] || p[0]=='.' && !s.empty())
+                return (isMatch(s,p.substr(2)) || isMatch(s.substr(1),p));
+            else
+                return isMatch(s, p.substr(2));
         }
-        while (!s.empty() && (s[0] == p[0] || p[0] == '.')) {
-            if (isMatch(s, p.substr(2))) return true;
-            s = s.substr(1);
-        }
-        return isMatch(s, p.substr(2));
     }
 };
 
