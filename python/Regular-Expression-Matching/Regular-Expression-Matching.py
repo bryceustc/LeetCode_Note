c560cf36b01a67f5254dd4628e47b6dbc78e3077
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-28 11:17:09 +0800

    Update Regular-Expression-Matching.py

--- python/Regular-Expression-Matching/Regular-Expression-Matching.py
+++ python/Regular-Expression-Matching/Regular-Expression-Matching.py
@@ -17,3 +17,24 @@ class Solution:
             return self.isMatch(s[1:],p[1:])
         else:
             return False
+### Solution 2:
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        n = len(s)
+        m = len(p)
+        import numpy
+        dp = numpy.zeros((n+1,m+1)) 
+        ### python 二维数组初始化  dp[[0 for _ in range(m+2)] for _ in range(n+2)]
+        s = ' ' + s
+        p = ' ' + p
+        dp[0][0] = 1
+        for i in range(n+1):
+            for j in range(1,m+1):
+                if i > 0 and (s[i]==p[j] or p[j]=='.'):
+                    dp[i][j] = dp[i][j] or dp[i-1][j-1]
+                if p[j]=='*':
+                    if j>=2:
+                        dp[i][j] = dp[i][j] or dp[i][j-2]
+                    if i > 0 and (s[i]==p[j-1] or p[j-1]=='.'):
+                        dp[i][j] = dp[i][j] or dp[i-1][j]
+        return dp[n][m]
