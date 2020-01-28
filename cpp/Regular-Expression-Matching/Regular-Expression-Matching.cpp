6629cf72e7695153198d0f040317ad9f9c165ce8
bryce <33057792+bryceustc@users.noreply.github.com>
2020-01-28 11:16:40 +0800

    Update Regular-Expression-Matching.cpp

--- cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
+++ cpp/Regular-Expression-Matching/Regular-Expression-Matching.cpp
@@ -29,22 +29,33 @@ public:
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int n = s.size(), m = p.size();
-        vector<vector<bool>> f(n + 1, vector<bool>(m + 1, false));
-        s = " " + s;
-        p = " " + p;
-        f[0][0] = true;
-        for (int i = 0; i <= n; i++)
-            for (int j = 1; j <= m; j++) {
-                if (i > 0 && (s[i] == p[j] || p[j] == '.'))
-                    f[i][j] = f[i][j] | f[i - 1][j - 1];
-                if (p[j] == '*') {
-                    if (j >= 2)
-                        f[i][j] = f[i][j] | f[i][j - 2];
-                    if (i > 0 && (s[i] == p[j - 1] || p[j - 1] == '.'))
-                        f[i][j] = f[i][j] | f[i - 1][j];
+        int n = s.size();
+        int m = p.size();
+        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        s = ' '+s;
+        p = ' '+p;
+        for (int i=0;i<=n;i++)  // i从0开始，因为dp(0,j)有可能是有意义的
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if (i>0 && (s[i]==p[j] || p[j]=='.'))
+                {
+                    dp[i][j] = dp[i][j] | dp[i-1][j-1];  // | 表示按位或
+                }
+                if (p[j]=='*')
+                {
+                    if (j>=2)
+                    {
+                        dp[i][j] = dp[i][j] | dp[i][j-2];
+                    }
+                    if (i>0 && (s[i]==p[j-1] || p[j-1]=='.'))
+                    {
+                        dp[i][j] = dp[i][j] | dp[i-1][j];
+                    }
                 }
             }
-        return f[n][m];
+        }
+        return dp[n][m];        
     }
 };
