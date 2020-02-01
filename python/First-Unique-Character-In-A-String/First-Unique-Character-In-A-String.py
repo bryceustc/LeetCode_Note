954b9df4b22a7d58a5352a67c0f33c79ceb3713f
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-01 20:18:23 +0800

    Update First-Unique-Character-In-A-String.py

--- python/First-Unique-Character-In-A-String/First-Unique-Character-In-A-String.py
+++ python/First-Unique-Character-In-A-String/First-Unique-Character-In-A-String.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        n = len(s)
+        res = -1
+        if n==0:
+            return res
+        count = collections.Counter(s)
+        for index,ch in enumerate(s):
+            if count[ch]==1:
+                return index
+        return res
