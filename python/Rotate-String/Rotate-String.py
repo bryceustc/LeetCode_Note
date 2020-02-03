8ce5a3c46e7d1424cf593439ba585e7c4edde324
bryce <33057792+bryceustc@users.noreply.github.com>
2020-02-03 17:57:56 +0800

    Update Rotate-String.py

--- python/Rotate-String/Rotate-String.py
+++ python/Rotate-String/Rotate-String.py
@@ -1,1 +1,36 @@
-
+class Solution:
+    def rotateString(self, A: str, B: str) -> bool:
+        n = len(A)
+        m = len(B)
+        if n!=m:
+            return False
+        if A=="" and B=="":
+            return True
+        for i in range(n):
+            A = self.leftmove(A)
+            if A==B:
+                return True
+        return False
+    
+    def leftmove(self,s):
+        s = list(s)
+        n = len(s)
+        temp = s[0]
+        for i in range(n):
+            if i!=n-1:
+                s[i]=s[i+1]
+            else:
+                s[i]=temp
+        return ''.join(s)
+## Solution 2：
+class Solution:
+    def rotateString(self, A: str, B: str) -> bool:
+        n = len(A)
+        m = len(B)
+        if n!=m:
+            return False
+        if A=="" and B == "":
+            return True
+        if B in (A+A):
+            return True
+        return False
