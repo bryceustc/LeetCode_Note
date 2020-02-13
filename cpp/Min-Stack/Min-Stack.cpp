f9b08221626e69ad4c294d64feec4b6c4e430d52
bryce <bryce@mail.ustc.edu.cn>
2020-02-13 16:09:22 +0800

    Update Min-Stack.cpp

--- cpp/Min-Stack/Min-Stack.cpp
+++ cpp/Min-Stack/Min-Stack.cpp
@@ -1,1 +1,48 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        stack1.push(x);
+        if (stack2.empty() || x < stack2.top())
+        {
+            stack2.push(x);
+        }
+        else
+        {
+            int y = stack2.top();
+            stack2.push(y);
+        }
+    }
+    
+    void pop() {
+        if (stack1.size()>0 && stack2.size()>0)
+        {
+            stack1.pop();
+            stack2.pop();
+        }
+    }
+    
+    int top() {
+        return stack1.top();
+    }
+    
+    int getMin() {
+        return stack2.top();
+    }
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
