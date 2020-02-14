bb9cd1f08aabb4645e2d46d3019ead76e1d31053
bryce <bryce@mail.ustc.edu.cn>
2020-02-14 16:30:32 +0800

    Update Validate-Stack-Sequences.py

--- python/Validate-Stack-Sequences/Validate-Stack-Sequences.py
+++ python/Validate-Stack-Sequences/Validate-Stack-Sequences.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        n = len(pushed)
+        m = len(popped)
+        if n!=m:
+            return False
+        s = []
+        index = 0
+        for i in range(n):
+            s.append(pushed[i])
+            while len(s)>0 and s[-1]==popped[index]:
+                s.pop()
+                index+=1
+        return len(s)==0
