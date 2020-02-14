af17da53456cab19d2e6672c91bc8bd178ecd0e2
bryce <bryce@mail.ustc.edu.cn>
2020-02-14 16:29:10 +0800

    Update Validate-Stack-Sequences.cpp

--- cpp/Validate-Stack-Sequences/Validate-Stack-Sequences.cpp
+++ cpp/Validate-Stack-Sequences/Validate-Stack-Sequences.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        int m = popped.size();
+        if (n!=m) return false;
+        stack<int> s;
+        int index = 0;
+        for (int i=0;i<n;i++)
+        {
+            s.push(pushed[i]);
+            while(!s.empty() && s.top()==popped[index])
+            {
+                s.pop();
+                index++;
+            }
+        }
+        return s.empty();
+    }
+};
