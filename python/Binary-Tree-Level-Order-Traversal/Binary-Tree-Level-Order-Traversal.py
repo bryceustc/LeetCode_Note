d5b7bebec14a21f1b0b4237644fa7e48645b43b8
bryce <bryce@mail.ustc.edu.cn>
2020-02-19 18:03:49 +0800

    Update Binary-Tree-Level-Order-Traversal.py

--- python/Binary-Tree-Level-Order-Traversal/Binary-Tree-Level-Order-Traversal.py
+++ python/Binary-Tree-Level-Order-Traversal/Binary-Tree-Level-Order-Traversal.py
@@ -1,1 +1,45 @@
+## Python Solution 1:
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root==NULL)
+            return res;
+        helper(root, res,0);
+        return res;
+    }
 
+    void helper(TreeNode* root, vector<vector<int>> &res, int level)
+    {
+        if (root==NULL)
+            return;
+        if (res.size()==level)
+            res.push_back(vector<int>());
+        res[level].push_back(root->val);
+        if (root->left)
+            helper(root->left,res,level+1);
+        if (root->right)
+            helper(root->right,res,level+1);
+    }
+};
+## Python Solution 2:
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        if root is None:
+            return res
+        q = []
+        q.append(root)
+        while q:
+            n = len(q)
+            out = []
+            while n>0:
+                node = q.pop(0)
+                out.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                n-=1
+            res.append(out[:])
+        return res
