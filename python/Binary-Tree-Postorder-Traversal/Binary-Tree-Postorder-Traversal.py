38ccb5c0d27b42ac9d803ba08982aad2a097d4c3
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 20:14:01 +0800

    Update Binary-Tree-Postorder-Traversal.py

--- python/Binary-Tree-Postorder-Traversal/Binary-Tree-Postorder-Traversal.py
+++ python/Binary-Tree-Postorder-Traversal/Binary-Tree-Postorder-Traversal.py
@@ -1,1 +1,31 @@
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if root == None:
+            return res
+        self.helper(root,res)
+        return res
+    def helper(self,root,res):
+        if root is None:
+            return
+        self.helper(root.left,res)
+        self.helper(root.right,res)
+        res.append(root.val)
 
+        
+        
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if root is None:
+            return res
+        s = []
+        s.append(root)
+        while s:
+            node = s.pop()
+            if node.left:
+                s.append(node.left)
+            if node.right:
+                s.append(node.right)
+            res.append(node.val)
+        return res[::-1]
