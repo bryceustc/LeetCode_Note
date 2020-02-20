6be5e03faa725eff62e3ea0233a7ca510d9c5df4
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 20:41:15 +0800

    Update Binary-Tree-Inorder-Traversal.cpp

--- cpp/Binary-Tree-Inorder-Traversal/Binary-Tree-Inorder-Traversal.cpp
+++ cpp/Binary-Tree-Inorder-Traversal/Binary-Tree-Inorder-Traversal.cpp
@@ -1,1 +1,43 @@
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root==NULL)
+            return res;
+        helper(root,res);
+        return res;
+    }
+    void helper(TreeNode* root, vector<int> &res)
+    {
+        if (root==NULL)
+            return;
+        helper(root->left,res);
+        res.push_back(root->val);
+        helper(root->right,res);
+    }
+};
 
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>  res;
+        if (root==NULL)
+            return res;
+        stack<TreeNode*> s;
+        TreeNode* p = root;
+        while (!s.empty()||p!=NULL)
+        {
+            while(p!=NULL)
+            {
+                s.push(p);
+                p = p->left;
+            }
+            p = s.top();
+            s.pop();
+            res.push_back(p->val);
+            p = p->right;
+        }
+        return res;
+    }
+};
