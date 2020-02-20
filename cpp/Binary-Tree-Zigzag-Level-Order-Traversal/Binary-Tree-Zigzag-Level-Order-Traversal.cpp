c42601a0b436be0cea034ffc24d8b1c59cf751cc
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 17:15:38 +0800

    Update Binary-Tree-Zigzag-Level-Order-Traversal.cpp

--- cpp/Binary-Tree-Zigzag-Level-Order-Traversal/Binary-Tree-Zigzag-Level-Order-Traversal.cpp
+++ cpp/Binary-Tree-Zigzag-Level-Order-Traversal/Binary-Tree-Zigzag-Level-Order-Traversal.cpp
@@ -1,1 +1,43 @@
-
+class Solution {
+public:
+    vector<vector<int>> zigzaglevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        if (root==NULL)
+            return res;
+        bool flag = true; //从左向右打印为true，从右向左打印为false
+        deque<TreeNode*> q;
+        q.push_back(root);
+        while (!q.empty())
+        {
+            int n = q.size();
+            vector<int> out;
+            TreeNode* node;
+            while (n>0)
+            {
+                if (flag) // 前取后放：从左向右打印，所以从前边取，后边放入
+                {
+                    node = q.front();
+                    q.pop_front();
+                    if (node->left)
+                        q.push_back(node->left);  // 下一层顺序存放至尾
+                    if (node->right)
+                        q.push_back(node->right);
+                }
+                else  //后取前放： 从右向左，从后边取，前边放入
+                {
+                    node = q.back();
+                    q.pop_back();
+                    if (node->right)
+                        q.push_front(node->right);  // 下一层逆序存放至首
+                    if (node->left)
+                        q.push_front(node->left);
+                }
+                out.push_back(node->val);
+                n--;
+            }
+            flag = !flag;
+            res.push_back(out);
+        }
+        return res;
+    }
+};
