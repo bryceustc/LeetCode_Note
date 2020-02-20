d6f347af04550d7b5262ce9b3d3f372646afaa9b
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 20:13:28 +0800

    Update Binary-Tree-Postorder-Traversal.cpp

--- cpp/Binary-Tree-Postorder-Traversal/Binary-Tree-Postorder-Traversal.cpp
+++ cpp/Binary-Tree-Postorder-Traversal/Binary-Tree-Postorder-Traversal.cpp
@@ -1,1 +1,42 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root==NULL)
+            return res;
+        helper(root,res);
+        return res;
+    }
+    void helper(TreeNode* root, vector<int> &res)
+    {
+        if (root==NULL)
+            return;
+        helper(root->left,res);
+        helper(root->right,res);
+        res.push_back(root->val);
+    }
+};
 
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root==NULL)
+            return res;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!s.empty())
+        {
+            TreeNode* node = s.top();
+            s.pop();
+            if (node->left)
+                s.push(node->left);
+            if (node->right)
+                s.push(node->right);
+            res.push_back(node->val);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
