85de4741c8aa01550c2f9ea3808646d617ec30a7
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 20:42:08 +0800

    Update Binary-Tree-Inorder-Traversal.py

--- python/Binary-Tree-Inorder-Traversal/Binary-Tree-Inorder-Traversal.py
+++ python/Binary-Tree-Inorder-Traversal/Binary-Tree-Inorder-Traversal.py
@@ -1,1 +1,30 @@
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if root == None:
+            return res
+        self.helper(root,res)
+        return res
+    def helper(self,root,res):
+        if root is None:
+            return
+        self.helper(root.left,res)
+        res.append(root.val)
+        self.helper(root.right,res)
 
+        
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if root is None:
+            return res
+        s = []
+        p = root
+        while p or s:
+            while p!=None:
+                s.append(p)
+                p = p.left
+            p = s.pop()
+            res.append(p.val)
+            p = p.right
+        return res
