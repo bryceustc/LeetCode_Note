95ae9e89f775983cc68e0bf87f5d4b853d08a333
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 17:42:58 +0800

    Update Binary-Tree-Preorder-Traversal.py

--- python/Binary-Tree-Preorder-Traversal/Binary-Tree-Preorder-Traversal.py
+++ python/Binary-Tree-Preorder-Traversal/Binary-Tree-Preorder-Traversal.py
@@ -1,1 +1,15 @@
-
+## Solution 1:
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        if root is None:
+            return res
+        self.helper(root, res)
+        return res
+    def helper(self, root, res):
+        if root is None:
+            return
+        res.append(root.val)
+        self.helper(root.left, res)
+        self.helper(root.right, res)
+## Solution 2：
