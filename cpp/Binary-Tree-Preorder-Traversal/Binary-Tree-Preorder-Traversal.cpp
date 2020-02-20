db0d5e35f8f63d313620b3dbdb7723d145e02b8c
bryce <bryce@mail.ustc.edu.cn>
2020-02-20 19:43:47 +0800

    Update Binary-Tree-Preorder-Traversal.cpp

--- cpp/Binary-Tree-Preorder-Traversal/Binary-Tree-Preorder-Traversal.cpp
+++ cpp/Binary-Tree-Preorder-Traversal/Binary-Tree-Preorder-Traversal.cpp
@@ -1,1 +1,46 @@
-
+// Solution 1:
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root==NULL)
+            return res;
+        helper(root,res);
+        return res;
+    }
+    void helper(TreeNode* root, vector<int> &res)
+    {
+        if (root==NULL)
+            return;
+        res.push_back(root->val);
+        helper(root->left,res);
+        helper(root->right,res);
+    }
+};
+// Solution 2:
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root==NULL)
+            return res;
+        stack<TreeNode*> s;
+        s.push(root);
+        while (!q.empty())
+        {
+            TreeNode* node = s.top();
+            s.pop();
+            res.push_back(node->val);
+            if (node->right)
+            {
+                s.push(node->right);
+            }
+            if (node->left)
+            {
+                s.push(node->left);
+            }
+            
+        }
+        return res;
+    }
+};
