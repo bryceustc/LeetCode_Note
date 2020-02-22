f70a762323d49f70569a699454bf715c5b94b2d2
bryce <bryce@mail.ustc.edu.cn>
2020-02-22 20:49:13 +0800

    Update Path-Sum-II.cpp

--- cpp/Path-Sum-II/Path-Sum-II.cpp
+++ cpp/Path-Sum-II/Path-Sum-II.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> res;
+        if (root == NULL)
+            return res;
+        vector<int> path;
+        dfs(root, res, path, sum);
+        return res;
+    }
+    void dfs(TreeNode* root, vector<vector<int>> &res, vector<int> &path, int sum)
+    {
+        if (root == NULL)
+            return;
+        path.push_back(root->val);
+        bool isLeaf = (root->left == NULL) && (root->right == NULL);
+        if (sum == root->val && isLeaf)
+        {
+            res.push_back(path);
+        }
+        dfs(root->left, res, path, sum - root->val);
+        dfs(root->right, res, path,sum - root->val);
+        path.pop_back();
+    }
+};
