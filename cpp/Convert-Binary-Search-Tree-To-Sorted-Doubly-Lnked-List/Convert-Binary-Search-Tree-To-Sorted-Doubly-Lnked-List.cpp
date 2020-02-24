b274080b4830ba538cae3565d7937669935a65dd
bryce <bryce@mail.ustc.edu.cn>
2020-02-24 11:28:35 +0800

    Update Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List.cpp

--- cpp/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List.cpp
+++ cpp/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    Node* treeToDoublyList(Node* root) {
+        if (root==NULL)
+            return NULL;
+        Node* pre = NULL;
+        helper(root, pre);
+        Node* head =pre;
+        while(head && head->left)
+        {
+            head = head->left;
+        }
+        pre -> right = head;
+        head->left = pre;
+        return head;
+    }
+    void helper(Node* cur, Node* &pre)
+    {
+        if (cur==NULL)
+            return;
+        helper(cur->left, pre);
+        cur->left = pre;
+        if (pre!=NULL)
+            pre->right = cur;
+        pre = cur;
+        helper(cur->right, pre);
+    }
+};
