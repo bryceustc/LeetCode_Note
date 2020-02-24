5ab2b5b878c54341eaf0984d9a628102d46f90d2
bryce <bryce@mail.ustc.edu.cn>
2020-02-24 16:05:39 +0800

    Update Path-Sum.py

--- python/Path-Sum/Path-Sum.py
+++ python/Path-Sum/Path-Sum.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root is None:
+            return False
+        isLeaf = (root.left==None) and (root.right==None)
+        sum -= root.val
+        if isLeaf:
+            return sum == 0
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
