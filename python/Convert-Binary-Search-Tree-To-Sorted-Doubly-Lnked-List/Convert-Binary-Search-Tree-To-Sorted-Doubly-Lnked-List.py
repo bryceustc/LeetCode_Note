1e50fb7518eb580df6ddc62763866a3e022004d6
bryce <bryce@mail.ustc.edu.cn>
2020-02-24 11:29:35 +0800

    Update Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List.py

--- python/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List.py
+++ python/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List/Convert-Binary-Search-Tree-To-Sorted-Doubly-Lnked-List.py
@@ -1,1 +1,21 @@
-
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        if root  is None:
+            return None
+        pre = None
+        pre = self.helper(root, pre)
+        head = pre
+        while head and head.left:
+            head = head.left
+        pre.right = head
+        head.left = pre
+        return head
+    def helper(self, cur,  pre):
+        if cur == None:
+            return pre
+        pre = self.helper(cur.left, pre)
+        cur.left = pre
+        if pre:
+            pre.right = cur
+        pre = cur
+        return self.helper(cur.right, pre)
