0a13cab8d80cc78eb80fe60d655dc16a5e7010a3
bryce <bryce@mail.ustc.edu.cn>
2020-02-27 14:46:42 +0800

    Update Maximum-Depth-Of-Binary-Tree.py

--- python/Maximum-Depth-Of-Binary-Tree/Maximum-Depth-Of-Binary-Tree.py
+++ python/Maximum-Depth-Of-Binary-Tree/Maximum-Depth-Of-Binary-Tree.py
@@ -1,1 +1,25 @@
-
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right)+1
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        depth = 0
+        if root is None:
+            return depth
+        q = []
+        q.append(root)
+        while q:
+            count = len(q)
+            while count >0:
+                node = q.pop(0)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                count -=1
+            depth+=1
+        return depth
