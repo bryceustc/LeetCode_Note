4e9aa78ac182205b05803b044b7d5eb6749b72ca
bryce <bryce@mail.ustc.edu.cn>
2020-02-28 15:54:21 +0800

    Update Number-Of-1-Bits.cpp

--- cpp/Number-Of-1-Bits/Number-Of-1-Bits.cpp
+++ cpp/Number-Of-1-Bits/Number-Of-1-Bits.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n!=0)
+        {
+            res++;
+            n = (n-1)&n;
+        }
+        return res;
+    }
+};
