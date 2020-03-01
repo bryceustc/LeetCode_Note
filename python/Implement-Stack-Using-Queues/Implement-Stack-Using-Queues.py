f2d9748486873c769a5ad170eabd5bc719039bdf
bryce <bryce@mail.ustc.edu.cn>
2020-03-01 15:48:57 +0800

    Update Implement-Stack-Using-Queues.py

--- python/Implement-Stack-Using-Queues/Implement-Stack-Using-Queues.py
+++ python/Implement-Stack-Using-Queues/Implement-Stack-Using-Queues.py
@@ -1,1 +1,35 @@
+class MyStack:
 
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.q = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.q.append(x)
+        for i in range(len(self.q)-1):
+            self.q.append(self.q.pop(0))
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        return self.q.pop(0)
+
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        return self.q[0]
+
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        n = len(self.q)
+        return n==0
