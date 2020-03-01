0411c469f1f0e16b1467487ba98332ba45a644d9
bryce <bryce@mail.ustc.edu.cn>
2020-03-01 13:30:14 +0800

    Update Rank-Teams-By-Votes.cpp

--- cpp/Rank-Teams-By-Votes/Rank-Teams-By-Votes.cpp
+++ cpp/Rank-Teams-By-Votes/Rank-Teams-By-Votes.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        string res = votes[0];
+        int n = votes[0].size();
+        map<char, map<int, int>> m;
+        for (auto s : votes)
+        {
+            for (int i=0;i<n;i++)
+            {
+                m[s[i]][i]++;
+            }
+        }
+        // [&] 任何被使用到的外部变量都隐式地以引用方式加以引用。
+        sort (res.begin(), res.end(), [&] (char a, char b)->bool
+        {
+            for (int i=0;i<n;i++)
+            {
+                if(m[a][i]  > m[b][i]) return true;
+                if (m[a][i] < m[b][i]) return false;
+            }
+            return a<b;
+        });
+        return res;
+    }
+};
