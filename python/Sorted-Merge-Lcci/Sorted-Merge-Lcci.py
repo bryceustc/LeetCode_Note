f197b1509137c727a56e1e3c2e64e820b9d93852
bryce <bryce@mail.ustc.edu.cn>
2020-03-03 11:15:02 +0800

    Update Sorted-Merge-Lcci.py

--- python/Sorted-Merge-Lcci/Sorted-Merge-Lcci.py
+++ python/Sorted-Merge-Lcci/Sorted-Merge-Lcci.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def merge(self, A: List[int], m: int, B: List[int], n: int) -> None:
+        """
+        Do not return anything, modify A in-place instead.
+        """
+        i = m-1
+        j = n-1
+        index = m+n-1
+        while i>=0 and j>=0:
+            if A[i] >= B[j]:
+                A[index] = A[i]
+                index-=1
+                i-=1
+            else:
+                A[index] = B[j]
+                index -=1
+                j-=1
+        # 比较完B还有剩下的，全填到A前面即可
+        A[:j+1] = B[:j+1]
