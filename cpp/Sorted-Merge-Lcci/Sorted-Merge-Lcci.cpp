878eefa1c66c69962263952b31270ab29e0d9182
bryce <bryce@mail.ustc.edu.cn>
2020-03-03 11:14:48 +0800

    Update Sorted-Merge-Lcci.cpp

--- cpp/Sorted-Merge-Lcci/Sorted-Merge-Lcci.cpp
+++ cpp/Sorted-Merge-Lcci/Sorted-Merge-Lcci.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    void merge(vector<int>& A, int m, vector<int>& B, int n) {
+        int i = m-1;
+        int j = n-1;
+        int index =  m+n-1;
+        while (i>=0 && j>=0)
+        {
+            if(A[i]>=B[j])
+            {
+                A[index--] = A[i--];
+            }
+            else
+            {
+                A[index--] = B[j--];
+            }
+        }
+        // 下面这个while没必要写，这时候index 和i是相等的。
+        // while (i>=0)  A[index--] = A[i--];
+        while (j>=0)  A[index--] = B[j--];
+    }
+};
