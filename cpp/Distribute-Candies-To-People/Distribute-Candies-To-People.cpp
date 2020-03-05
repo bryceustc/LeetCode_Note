a54f1d09a1b1d5975fde1b87fbdeef741b3ee57e
bryce <bryce@mail.ustc.edu.cn>
2020-03-05 14:45:38 +0800

    Update Distribute-Candies-To-People.cpp

--- cpp/Distribute-Candies-To-People/Distribute-Candies-To-People.cpp
+++ cpp/Distribute-Candies-To-People/Distribute-Candies-To-People.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
+        vector<int> res(num_people,0);
+        int i =0;
+        while (candies>i)
+        {
+            res[i%num_people] +=  i+1;
+            candies-=i+1;
+            i++;
+        }
+        res[i%num_people] += candies;
+        return res;
+    }
+};
