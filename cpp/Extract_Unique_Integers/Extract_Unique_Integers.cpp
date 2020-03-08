cc4bdb72d151003d82d4be1ee76b623037aa13e1
bryce <bryce@mail.ustc.edu.cn>
2020-03-08 22:50:27 +0800

    Update Extract_Unique_Integers.cpp

--- cpp/Extract_Unique_Integers/Extract_Unique_Integers.cpp
+++ cpp/Extract_Unique_Integers/Extract_Unique_Integers.cpp
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main()
+{
+    int input;
+    scanf("%d", &input);
+    int res = 0;
+    int m[10] = {0};
+    int n = 0;
+    while(input)
+    {
+        n = input%10;
+        if (m[n]==0)
+        {
+            m[n]++;
+            res = res*10 + n;
+        }
+        input/=10;
+    }
+    printf("%d",res);
+    return 0;
+}
