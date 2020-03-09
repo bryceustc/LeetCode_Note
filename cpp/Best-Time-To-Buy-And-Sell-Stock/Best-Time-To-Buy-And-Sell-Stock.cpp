787b0b7c5f23dd3d458658405e4a78b144adcc50
bryce <bryce@mail.ustc.edu.cn>
2020-03-09 16:46:43 +0800

    Update Best-Time-To-Buy-And-Sell-Stock.cpp

--- cpp/Best-Time-To-Buy-And-Sell-Stock/Best-Time-To-Buy-And-Sell-Stock.cpp
+++ cpp/Best-Time-To-Buy-And-Sell-Stock/Best-Time-To-Buy-And-Sell-Stock.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n<=1) return 0;
+        int res = 0;
+        int min_price = prices[0];
+        for (int i=0;i<n;i++)
+        {
+            res = max(res, prices[i] - min_price);
+            min_price = min(min_price, prices[i]);
+        }
+        return res;
+    }
+};
